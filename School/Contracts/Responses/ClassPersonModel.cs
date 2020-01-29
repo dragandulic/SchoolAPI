3bd7be0bd81b0221fe9d429992f93dd785431c76
jurke95 <markivicius@hotmail.com>
2020-01-29 03:49:15 +0100

    subjects controller

---
+++ School/Contracts/Responses/ClassPersonModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace School.Contracts.Responses
+{
+    public class ClassPersonModel
+    {
+        public long ClassId { get; set; }
+        public long PersonId { get; set; }
+        public string Grades { get; set; }
+    }
+}
---
+++ School/Controllers/SubjectsController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using School.Contracts.Responses;
+using School.Data;
+
+namespace School.Controllers
+{
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class SubjectsController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SchoolContext _context;
+
+
+        public SubjectsController(UserManager<ApplicationUser> userManager, SchoolContext context)
+        {
+            _userManager = userManager;
+            _context = context;
+
+        }
+
+        [HttpGet("api/GetSubjectsForUser")]
+        public async Task<IActionResult> GetSubjectsForUser()
+        {
+            var user = await _userManager.FindByNameAsync(_userManager.GetUserId(HttpContext.User));
+            var class_person = _context.ClassPerson.Where(c => c.PersonId == user.PersonId).Select(s => new {s.Mark, s.ClassId, s.PersonId }).Single();
+            string grades = class_person.Mark;
+            Console.WriteLine(grades);
+            var response = new ClassPersonModel();
+            response.ClassId = class_person.ClassId;
+            response.PersonId = class_person.PersonId;
+            response.Grades = grades;
+            return  Json(response);
+
+        }
+    }
+}
